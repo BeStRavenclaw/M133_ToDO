8fd72f06713fec7f6b347e17bd355a83c2862f41
Ben Stadelmann <ben.stadelmann2@gmail.ch>
2022-10-24 17:18:13 +0200

    Task 7

---
+++ Aufgabe_7/todo.js
@@ -0,0 +1,56 @@
+export class ToDo {
+  #titel = "";
+  #erledigt = "";
+
+  constructor(titel, erledigt) {
+    this.#titel = titel;
+    this.#erledigt = erledigt;
+  }
+
+  get titel() {
+    return this.#titel;
+  }
+
+  set titel(titel) {
+    this.#titel = titel;
+  }
+
+  get erledigt() {
+    return this.#erledigt;
+  }
+
+  set erledigt(erledigt) {
+    this.#erledigt = erledigt;
+  }
+
+  element() {
+    const listElement = document.createElement("li");
+    const divElement = document.createElement("div");
+    const checkboxElement = document.createElement("input");
+    const spanElement = document.createElement("span");
+      const buttonElement = document.createElement("button");
+      buttonElement.addEventListener("click", function (e) {
+          e.target.parentElement.remove();
+      });
+
+    listElement.appendChild(divElement);
+
+    divElement.appendChild(checkboxElement);
+    divElement.appendChild(spanElement);
+    divElement.appendChild(buttonElement);
+
+    checkboxElement.setAttribute("type", "checkbox");
+
+    buttonElement.className = "loeschen";
+
+    spanElement.innerText = this.#titel;
+    buttonElement.innerText = "Löschen";
+
+    if (this.#erledigt) {
+      checkboxElement.setAttribute("checked", "checked");
+      divElement.className = "erledigt";
+    }
+
+    return listElement;
+  }
+}
